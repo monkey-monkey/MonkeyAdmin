0b2116a10b9ee5dfdc4a29c7fa9162a84547836e
chutipon29301 <chutiponh@hotmail.com>
2017-01-14 14:13:03 +0700

    Add level button cteated using for loop

--- src/Menu.java
+++ src/Menu.java
@@ -10,12 +10,14 @@ import java.util.ArrayList;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.Color;
+import javax.swing.JToggleButton;
 
 public class Menu extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
 	private ArrayList<JButton> actionBtn = new ArrayList<JButton>();
+	private ArrayList<JToggleButton> levelBtn = new ArrayList<JToggleButton>();
 	
 	/**
 	 * Launch the application.
@@ -61,6 +63,32 @@ public class Menu extends JFrame {
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
+//		JToggleButton tglbtnMj = new JToggleButton("MJ");
+//		tglbtnMj.setBackground(new Color(255, 204, 0));
+//		tglbtnMj.setFont(new Font("Cordia New", Font.PLAIN, 80));
+//		tglbtnMj.setBounds(100, 250, 114, 96);
+//		contentPane.add(tglbtnMj);
+		
+		
+		levelBtn.add(new JToggleButton("MK"));
+		levelBtn.add(new JToggleButton("MJ"));
+		levelBtn.add(new JToggleButton("MH"));
+		levelBtn.add(new JToggleButton("MI"));
+		levelBtn.add(new JToggleButton("PJ"));
+		levelBtn.add(new JToggleButton("PH"));
+		levelBtn.add(new JToggleButton("PI"));
+		
+		for (int i = 0; i < levelBtn.size(); i++) {
+			levelBtn.get(i).setBackground(new Color(255, 204, 0));
+			levelBtn.get(i).setFont(new Font("Cordia New", Font.PLAIN, 80));
+			levelBtn.get(i).setBounds(100, 200 + (i * 110), 120, 90);
+			contentPane.add(levelBtn.get(i));
+		}
+		
+		
+		/**
+		 * Create object action Button
+		 */
 		actionBtn.add(new JButton("Print & VDO"));
 		actionBtn.add(new JButton("Print"));
 		actionBtn.add(new JButton("VDO"));
@@ -80,8 +108,15 @@ public class Menu extends JFrame {
 		actionBtn.get(1).setBounds(1451, 60, 151, 50);
 		actionBtn.get(2).setBounds(1692, 60, 151, 50);
 		
+		
+		/**
+		 * Add action button to content pane
+		 */
 		for (int i = 0; i < actionBtn.size(); i++) {
 			contentPane.add(actionBtn.get(i));
 		}
+		
+		
+		
 	}
 }
