20ec2319e2b7b748439d6e184c77c80ac0e9e470
chutipon29301 <chutiponh@hotmail.com>
2017-01-15 13:53:23 +0700

    Add copy method in FileUtil

--- src/FileUtil.java
+++ src/FileUtil.java
@@ -1,5 +1,9 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -48,9 +52,42 @@ public class FileUtil {
 		double revVal = 0.0;
 		for (int i = 0; i < commonFolderName.size(); i++) {
 			double tempVal = Double.parseDouble(commonFolderName.get(i).charAt(commonFolderName.get(i).indexOf('V') + 1) + "." + commonFolderName.get(i).charAt(commonFolderName.get(i).indexOf('_') + 1));
-			if (tempVal > revVal) index = i;
+			if (tempVal > revVal) {
+				index = i;
+				revVal = tempVal;
+			}
 		}
 		return commonFolderName.get(index);
+	}
+	
+	public boolean copy(String destinationPath) {
+		try {
+			copyFileUsingFileChannels(new File(path), new File(destinationPath));
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+	
+	private void copyFileUsingFileChannels(File source, File dest) throws IOException {
+		FileChannel inputChannel = null;
+		FileChannel outputChannel = null;
+		try {
+			inputChannel = inputExtracted(source).getChannel();
+			outputChannel = outputExtracted(dest).getChannel();
+			outputChannel.transferFrom(inputChannel, 0, inputChannel.size());
+		} finally {
+			inputChannel.close();
+			outputChannel.close();
+		}
+	}
+
+	private FileOutputStream outputExtracted(File dest) throws FileNotFoundException {
+		return new FileOutputStream(dest);
+	}
+
+	private FileInputStream inputExtracted(File source) throws FileNotFoundException {
+		return new FileInputStream(source);
 	}
 	
 	private void listFile() {
